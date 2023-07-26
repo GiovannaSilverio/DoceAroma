92ed0080f4269ad18e5cc76615356f9bd05d6cee
Giovanna <giosilverio.10@gmail.com>
2023-07-25 22:11:34 -0300

    add links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className=' h-full bg-marromescuro w-full'>
-      <div className='flex flex-row justify-between px-10'>
-      <div className='text-bege'>12 9999-9999</div>
-      <div  className='text-bege'>docearomacafe</div>
-      <div  className='text-bege'>@docearomacafe</div>
+    <footer className='h-300px bg-marromescuro w-full'>
+      <div className='flex flex-col items-center justify-center sm:flex-row sm:justify-between px-10'>
+        <div className='text-bege'>12 9999-9999</div>
+        <div className='text-bege'>docearomacafe</div>
+        <div className='text-bege'>@docearomacafe</div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
