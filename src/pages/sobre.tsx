f5721a1d1db6332441c1376872becca598ce35f0
Giovanna <giosilverio.10@gmail.com>
2023-07-25 21:32:56 -0300

    arrumando o que foi mudado nessa branch

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 export const Sobre = () => {
   return (
-    <section className='flex flex-col bg-marromedio sm:bg-marromedio h-screen flex items-center'>
-      <div className='sm:container flex justify-center'>
-        <div className='sm:w-1/3 '>
-          <picture><img className='sm:rounded-md h-3/3 m-20 w-full' src="frente.jpg" alt="imagem com a frente da cafeteria" /></picture>
+    <section className='flex flex-col sm:bg-marromedio w-full h-screen sm:flex-row'>
+
+        <div className='flex items-center w-full sm:bg-marrommedio sm:bg-marromedio sm:w-1/2 sm:h-screen'>
+          <picture className='h-full w-full sm:h-2/3 sm:m-10'>
+            <img className='sm:rounded-md h-full w-full object-cover' src="frente.jpg" alt="imagem com a frente da cafeteria" />
+          </picture>
         </div>
-        <div className='sm:w-2/3 flex flex-row justify-center items-center'>
-          <article className='w-2/3'>
-            <h1 className='text-center text-bege'>SOBRE NÓS</h1>
-            <p className='text-right text-bege m-20'>A Doce Aroma é uma cafeteria que tem por objetivo ser um ambiente aconchegante unindo duas experiências: desfrutar de um delicioso café lendo um incrível livro.</p>
-            <p className='text-right text-bege m-20'>Pensando nisso, trouxemos um ambiente ideal para você ler seu livro favorito, com jazz de fundo e uma boa dose de café acompanhada da comida de sua preferência.</p>
+        <div className='flex items-center bg-marromedio w-full h-screen justify-center sm:bg-marromedio sm:w-1/2 sm:h-screen'>
+          <article className=''>
+            <h1 className='text-center text-bege gap-y-1'>SOBRE NÓS</h1>
+            <p className='text-center text-bege m-20'>A Doce Aroma é uma cafeteria que tem por objetivo ser um ambiente aconchegante unindo duas experiências: desfrutar de um delicioso café lendo um incrível livro.</p>
+            <p className='text-center text-bege m-20'>Pensando nisso, trouxemos um ambiente ideal para você ler seu livro favorito, com jazz de fundo e uma boa dose de café acompanhada da comida de sua preferência.</p>
           </article>
         </div>
-      </div>
+     
     </section>
   );
 };
